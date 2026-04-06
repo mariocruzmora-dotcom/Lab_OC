165900589ac93faabefd584c7921837616ba147f
mariocruzmora-dotcom <mario.cruz.mora@uabc.edu.mx>
2026-04-06 06:06:40 +0000

    todos los incisos completos

---
+++ P5/P5b.asm
@@ -0,0 +1,23 @@
+%include "../LIB/pc_io.inc"  	; incluir declaraciones de procedimiento externos
+								; que se encuentran en la biblioteca libpc_io.a
+
+section	.text
+	global _start       ;referencia para inicio de programa
+	
+_start:  
+mov edx, msg		; edx = dirección de la cadena msg
+	call puts  
+
+	mov esi, msg
+	mov byte [esi+23], 'X'
+
+	mov edx, msg		
+	call puts 
+
+	mov	eax, 1	    	; seleccionar llamada al sistema para fin de programa
+	int	0x80        	; llamada al sistema - fin de programa
+
+section	.data
+msg	db  'abcdefghijklmnopqrstuvwxyz0123456789',0xa,0 
+
+
