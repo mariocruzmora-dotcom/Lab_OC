d3edc983522c134c2bc6ca50945cb5a7a474c9e3
mariocruzmora-dotcom <mario.cruz.mora@uabc.edu.mx>
2026-04-07 00:22:44 +0000

    p6

---
+++ P6/P6a.asm
@@ -0,0 +1,23 @@
+%include "../LIB/pc_iox.inc"
+
+section	.text
+
+	global _start       ;must be declared for using gcc
+
+_start:                     ;tell linker entry point
+	
+	mov ebx,0x5C4B2A60 
+	;call pHex_dw
+
+	mov edx, 0x12345678
+
+	mov ebx, edx
+	call pHex_dw
+
+	mov al,10	; cambio de linea
+	call putchar
+	
+	mov eax, 1	;system call number (sys_exit) -- fin del programa
+	int 0x80        ;call kernel
+
+
