190b2633e0a2d117b1aa256e97c9880fea362fb6
mariocruzmora-dotcom <mario.cruz.mora@uabc.edu.mx>
2026-04-13 20:37:53 +0000

    Practica6

--- P6/P6a.asm
+++ P6/P6a.asm
@@ -1,23 +1,96 @@
 %include "../LIB/pc_iox.inc"
 
+section .data
+    N dw 0
+    
 section	.text
 
 	global _start       ;must be declared for using gcc
 
-_start:                     ;tell linker entry point
-	
-	mov ebx,0x5C4B2A60 
-	;call pHex_dw
+_start:                     
 
-	mov edx, 0x12345678
+; a
 
-	mov ebx, edx
-	call pHex_dw
+    mov ebx, 0x5C4B2A60
+    add ebx, 0x0021E5DC
 
-	mov al,10	; cambio de linea
+    mov eax, ebx
+    call pHex_dw
+
+; b
+    mov al,10 	; cambio de linea
 	call putchar
-	
-	mov eax, 1	;system call number (sys_exit) -- fin del programa
-	int 0x80        ;call kernel
 
+    push bx
 
+; c
+    mov al,10 	; cambio de linea
+	call putchar
+
+    mov al, bl
+    mov ah, 0
+    mov cl, 8
+    mul cl
+
+    mov [N], ax
+
+    mov eax, 0
+    mov ax, [N]
+    call pHex_dw
+
+; d
+    mov al,10 	; cambio de linea
+	call putchar
+
+    inc word [N]
+
+    mov eax, 0
+    mov ax, [N]
+    call pHex_dw
+
+; e
+
+    mov ax, bx
+    mov bl, 0xFF
+    div bl
+
+    mov al,10 	; cambio de linea
+	call putchar
+
+    ; imprimir cociente
+    movzx eax, al
+    call pHex_dw
+
+    mov al,10 	; cambio de linea
+	call putchar
+
+    ; imprimir residuo
+    movzx eax, ah
+    call pHex_dw
+
+; f
+
+    mov al,10 	; cambio de linea
+	call putchar
+
+    add [N], ah
+
+    mov eax, 0
+    mov ax, [N]
+    call pHex_dw
+
+; h
+
+    mov al,10 	; cambio de linea
+	call putchar
+    
+    pop ax
+
+    mov eax, 0
+    mov ax, ax
+    call pHex_dw
+
+;
+    mov eax, 1
+    mov ebx, 0
+    int 0x80
