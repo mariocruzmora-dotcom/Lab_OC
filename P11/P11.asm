09b3c0f427795731fbb21ed1a5d489f4011234eb
mariocruzmora-dotcom <mario.cruz.mora@uabc.edu.mx>
2026-05-20 05:43:48 +0000

    listo

--- P11/P11.asm
+++ P11/P11.asm
@@ -1,49 +1,130 @@
+section .data
+char db 0
+
 section .text
 
 global pBin8b
-
+global pBin16b
+global pBin32b
 
 my_putchar:
 
     pushad
 
-    push eax
+    mov [char], al
+
     mov eax,4
     mov ebx,1
-    mov ecx,esp
+    mov ecx,char
     mov edx,1
     int 80h
 
-    pop eax
     popad
     ret
-
 
 pBin8b:
 
     push ebp
     mov ebp, esp
 
-    mov bl, [ebp+8]   ; cargar dato
-    mov ecx, 8        ; 8 bits
+    push ebx
+    push ecx
 
-ciclo:
+    mov bl, [ebp+8]
+    mov ecx, 8
 
-    shl bl,1          ; mover bit al carry
-    jc imprimir1
+ciclo8:
+
+    shl bl,1
+    jc uno8
 
     mov al,'0'
     call my_putchar
-    jmp siguiente
+    jmp siguiente8
 
-imprimir1:
+uno8:
 
     mov al,'1'
     call my_putchar
 
-siguiente:
+siguiente8:
 
-    loop ciclo
+    loop ciclo8
+
+    pop ecx
+    pop ebx
+
+    mov esp, ebp
+    pop ebp
+    ret
+
+pBin16b:
+
+    push ebp
+    mov ebp, esp
+
+    push ebx
+    push ecx
+
+    mov bx, [ebp+8]
+    mov ecx, 16
+
+ciclo16:
+
+    shl bx,1
+    jc uno16
+
+    mov al,'0'
+    call my_putchar
+    jmp siguiente16
+
+uno16:
+
+    mov al,'1'
+    call my_putchar
+
+siguiente16:
+
+    loop ciclo16
+
+    pop ecx
+    pop ebx
+
+    mov esp, ebp
+    pop ebp
+    ret
+
+pBin32b:
+
+    push ebp
+    mov ebp, esp
+
+    push ebx
+    push ecx
+
+    mov ebx, [ebp+8]
+    mov ecx, 32
+
+ciclo32:
+
+    shl ebx,1
+    jc uno32
+
+    mov al,'0'
+    call my_putchar
+    jmp siguiente32
+
+uno32:
+
+    mov al,'1'
+    call my_putchar
+
+siguiente32:
+
+    loop ciclo32
+
+    pop ecx
+    pop ebx
 
     mov esp, ebp
     pop ebp
--- P11/test_P11.c
+++ P11/test_P11.c
@@ -2,10 +2,24 @@
 #include <stdint.h>
 
 void pBin8b(uint8_t dato);
+void pBin16b(uint16_t dato);
+void pBin32b(uint32_t dato);
 
 int main(){
 
+    printf("8 bits: ");
+    fflush(stdout);
     pBin8b(10);
+
+    printf("\n16 bits: ");
+    fflush(stdout);
+    pBin16b(10);
+
+    printf("\n32 bits: ");
+    fflush(stdout);
+    pBin32b(10);
+
+    printf("\n");
 
     return 0;
 }
